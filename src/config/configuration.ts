2f7fb2315c57e3a8d19de7925ebc6dc786cc87c6
TaiBTDevOps <95182618+TaiBT2@users.noreply.github.com>
2024-03-11 14:35:48 +0700

    Update configuration.ts

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -8,10 +8,10 @@ export const configuration = () => ({
     'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE. @ code test 1233 @ test code 1234 @ test code 1235 @ test code 1236 @ test code',
   port: parseInt(process.env.PORT, 10) || 4200,
   database: {
-    host: process.env.DATABASE_HOST || '10.20.22.68',
+    host: process.env.DATABASE_HOST || 'database-demo.postgres.database.azure.com',
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    username: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD || '123',
+    username: process.env.DATABASE_USERNAME || 'taibt',
+    password: process.env.DATABASE_PASSWORD || 'Laodai2023',
     database: process.env.DATABASE_NAME || 'training',
   },
 });
